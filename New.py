7a4ec69e147a123246f824f9ffeddf0ae26674eb
mukram12 <mukramshaikh12@gmail.com>
2023-09-24 19:06:21 +0530

    In This Project Showing Pre Defined Variables And User Defined Inputs. And Diferrence Type Of Sum.

---
+++ New.py
@@ -0,0 +1,14 @@
+#solution-1 (With Pre Defined Variables)
+
+num1 = 30
+num2 = 40
+sum = (num1+num2)
+print('the sum of given number is',sum)
+
+#Solution-2 (With User-Inputs)
+
+num1 = float(input('Enter a number here:'))
+num2 = float(input('Enter another number here:'))
+
+sum = num1+num2
+print("That The Sum Of the provide two numbers is",sum)
---
+++ main.py
@@ -0,0 +1,30 @@
+print("hello")
+def add():
+    print(2+2)
+
+def Sub():
+    print(10//2)
+
+def Mult():
+    print(5*6)
+
+def Div():
+    print(12/6)
+
+add()
+Sub()
+Mult()
+Div()
+
+add()
+add()
+
+def add():
+    return 5+2
+import mean from statistics
+a = [10,20,30,40,50,60,70,80,88,90,99,11,12]
+mean(a)
+
+print(1+2)
+
+
